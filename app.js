f2e5d63a0feb4c556582d6afd7dbda39f4030245
vinceye <vinceye.cm@gmail.com>
2023-10-09 17:02:56 -0700

    hello world login workflow added

--- app.js
+++ app.js
@@ -27,12 +27,75 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username })
+            if (!user) {
+                return done(null, false, { message: 'incorrect username' });
+            };
+            if (user.password !== password ) {
+                return done(null, false, { message: 'incorrect password' });
+            };
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch(err) {
+        done(err);
+    };
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', { user: req.user }));
+app.post('/log-in', 
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/',
+}));
+
+app.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+        return next(err);
+        }
+        res.redirect("/");
+    });
+});
+
 app.get('/sign-up-form', (req, res) => res.render('sign-up-form'));
+
+app.post('/sign-up-form', async (req, res, next) => {
+    try {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+        })
+
+        const result = await user.save();
+
+        res.redirect('/');
+    } catch(err) {
+        return next(err);
+    }
+})
 
 app.listen(3000, () => console.log('app listening on port 3000'));
 
